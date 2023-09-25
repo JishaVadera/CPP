a32cd565e039309307d78e3b67fa2c06d56ac1fa
unknown <jishamvadera04@gmail.com>
2023-09-25 16:27:33 +0530

    program of c++

---
+++ intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "My name is Jisha Vadera" <<endl;
+    cout << "I am 18 years old" <<endl;
+    cout << "I have complete diploma in computer eng" <<endl;
+    cout << "My hobby is dancing and singing" <<endl;
+    cout << "My birthplace is Surat" <<endl;
+    return 0;
+}
