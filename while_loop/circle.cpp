fc2b4aa4f06f4e215341b114969dc0cad16f486b
unknown <jishamvadera04@gmail.com>
2023-09-30 22:58:09 +0530

    program of cicle

---
+++ while_loop/circle.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class circle
+{
+    private:
+        int radius;
+
+    public:
+        int r = radius;
+       
+        void area(int r)
+        {
+            cout<<3.14*r*r;
+        }
+        void circ(int r)
+        {
+        cout<<2*3.14*r;
+        }
+};
+int main()
+{
+    int r1;
+    cin>>r1;
+
+    circle cir;
+    cir.area(r1);
+    cir.circ(r1);
+    return 0;
+}
